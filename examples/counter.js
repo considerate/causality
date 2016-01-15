1fae962c67c867d11a89fc63c2292de7cf3289ff
Viktor Erik Mikael Kronvall <viktor.kronvall@gmail.com>
2016-01-15 22:18:46 +0100

    Allow custom effect performers.
    Update examples.

--- examples/counter.js
+++ examples/counter.js
@@ -9,10 +9,11 @@ const {increment, incrementLater, incrementBy} = types;
 const waitAndInc = (ms) => delay(ms).then(() => Action(types.increment));
 const incLater = ms => Effect.all(
     [
-        Effect.call(waitAndInc, ms),
+        Effect.call(waitAndInc, ms*2), // : Effect Action
         Effect.call(waitAndInc, ms)
     ]
-).map(actions => {
+) // : Effect (List Action)
+.map(actions => { // List Action -> Action
     const count = actions.length;
     return Action(types.incrementBy, count);
 });
@@ -23,10 +24,11 @@ export const update = (state, action) => {
         return Result(state+1);
     }
     else if(type === incrementLater) {
-        return Result(state, incLater(1000));
+        const effect = incLater(100);
+        return Result(state, effect);
     }
     else if(type === incrementBy) {
         return Result(state+data);
     }
 };
-export const init = () => 0;
+export const init = () => Result(0, Effect.call(waitAndInc, 3000));
--- examples/main.js
+++ examples/main.js
@@ -1,33 +1,110 @@
 const assert = require('assert');
+import fetch from 'node-fetch';
 import {Effect, Types, Result, Action, perform} from '..';
 import {update as updateCounter, init as initCounter, types} from './counter.js';
 
+const actionTypes = Types('left', 'right', 'response', 'error');
+const {left,right} = actionTypes;
+const Left = Action.wrap(left);
+const Right = Action.wrap(right);
+
 const update = (state, action) => {
-    const {type} = action;
-    const side = type;
-    const unwrapped = Action.unwrap(action);
-    const before = state[side];
-    const {state: after, effect} = updateCounter(before,unwrapped);
-    state[side] = after;
-    const mainEffect = effect.map(Action.wrap(side));
-    return Result(state, mainEffect);
+    const {left,right,response,error} = actionTypes;
+    if(action.type === left) {
+        const unwrapped = Action.unwrap(action);
+        const {state: next, effect} = updateCounter(state.left, unwrapped);
+        state.left = next;
+        return Result(state, effect.map(Left));
+    } else if(action.type === right) {
+        const unwrapped = Action.unwrap(action);
+        const {state: next, effect} = updateCounter(state.right, unwrapped);
+        state.right = next;
+        return Result(state, effect.map(Right));
+    } else if(action.type === actionTypes.response) {
+        const {data} = action;
+        const {url, body} = data;
+        // Handle response data here
+        if(body.test === 'string data') {
+            state.left = state.left+5;
+            return Result(state);
+        } else {
+            return Result(state);
+        }
+    } else if (action.type === actionTypes.error) {
+        const {data: error} = action;
+        // Handle errors here
+        state.left -= 3;
+        return Result(state);
+    }
 };
 
-const init = () => ({
-    left: initCounter(),
-    right: initCounter(),
-});
+const effectTypes = Types('request');
+
+const init = () => {
+    const {state: leftState, effect: leftEff} = initCounter();
+    const {state: rightState, effect: rightEff} = initCounter();
+    const req = Effect(effectTypes.request, {
+        url: 'http://www.mocky.io/v2/56996086110000360c216ccc'
+    });
+    return Result(
+        {
+            left: leftState,
+            right: rightState,
+        },
+        Effect.all([
+            req,
+            leftEff.map(Left),
+            rightEff.map(Right)
+        ]) // : Effect (List Action)
+    );
+};
 
 const view = (state) => {
     console.log(state);
-}
+};
 
-const main = Effect.app({init, update, view});
+
+const performer = {
+    matcher(effect) {
+        if(effect.type === effectTypes.request) {
+            return this.request;
+        }
+    },
+    request: (effect) => {
+        const {data} = effect;
+        const {method, url, headers: h} = data;
+        const headers = Object.assign(h || {}, {method});
+        return fetch(url, headers)
+        .then(response => {
+            if(response.status >= 400 && response.status < 600) {
+                return [Action(error, {
+                    url,
+                    message: 'Failed to perform HTTP request',
+                    text: response.text
+                })];
+            } else {
+                return response.json().then(jsonData => {
+                    return [Action(actionTypes.response, {
+                        url,
+                        body: jsonData
+                    })];
+                });
+            }
+        })
+        .catch(err => {
+            return Action(actionTypes.error, {
+                url,
+                message: err.message,
+                stack: err.stack
+            });
+        });
+    },
+};
+
+const main = Effect.app({init, update, view, performer});
 const {increment, incrementLater} = types;
-const left = Action.wrap('left');
-const right = Action.wrap('right');
-main.next(left(Action(increment)));
-main.next(left(Action(increment)));
-main.next(left(Action(increment)));
-const act = left(Action(incrementLater));
+main.next(Left(Action(increment)));
+main.next(Right(Action(increment)));
+main.next(Left(Action(increment)));
+const act = Left(Action(incrementLater));
 main.next(act);

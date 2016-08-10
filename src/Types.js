da02d3b27020a201fa0241dc3a61fbd4d93d4baf
Viktor Kronvall <considerate.k@gmail.com>
2016-08-11 01:13:16 +0200

    Remove unused Types.js file.

--- src/Types.js
+++
@@ -1,8 +0,0 @@
-export const typeName = type => typeof type === 'symbol' ? Symbol.keyFor(type) : String(type);
-
-export const Types = (...list) => {
-    return list.reduce((obj, type) => {
-        obj[type] = type;//Symbol.for(type);
-        return obj;
-    },{});
-};
